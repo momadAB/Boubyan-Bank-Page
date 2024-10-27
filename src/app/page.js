c19cfe0922c689386231ed1d2f478c6852a1d039
unknown <abdulrahman.g.alfahad@gmail.com>
2024-10-27 09:21:45 +0300

    Part 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,73 @@
 import Image from "next/image";
+import Group56 from "../ImgAssets/Group56.png";
+import Group8 from "../ImgAssets/Group8.png";
+import Group19 from "../ImgAssets/Group19.png";
+import Group17 from "../ImgAssets/Group17.png";
+import Path2 from "../ImgAssets/Path2.png";
+import Path8 from "../ImgAssets/Path8.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex-col justify-items-center">
+      <div className="relative w-full flex justify-center items-center">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={Group56}
+          alt="Picture of Group56"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+          className="mt-48"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <Image
+          src={Group8}
+          alt="Picture of Group8"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "40%", height: "auto" }}
+          className="absolute top-18 left-40"
+        />
+      </div>
+      <div className="relative w-full flex flex-col items-center justify-center min-h-screen my-11">
+        <Image
+          src={Group19}
+          alt="Picture of Group19"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "80%", height: "auto" }}
+        />
+        <div className="my-24" />
+        <Image
+          src={Path2}
+          alt="Picture of Path2"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "10%", height: "auto" }}
+          className="absolute left-0"
+        />
+        <Image
+          src={Group17}
+          alt="Picture of Group17"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "80%", height: "auto" }}
+        />
+        <div>
+          <Image
+            src={Path8}
+            alt="Picture of Path8"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "10%", height: "auto" }}
+            className="absolute bottom-0 right-0"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
